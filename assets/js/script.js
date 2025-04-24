8f953fa6f8967d26366256988136635a37537818
RobertVLevesque <robertvlevesque@gmail.com>
2025-04-24 08:28:23 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,10 +9,12 @@ function skipCipher() {
     alert('Key appears. Proceed to site.');
 }
 function activateRLSigil() {
-    const triangle = document.getElementById("triangle-1");
+    const triangle1 = document.getElementById("triangle-1");
+    const triangle2 = document.getElementById("triangle-2");
     if (!localStorage.getItem("sigil_rl")) {
         localStorage.setItem("sigil_rl", "true");
-        triangle.style.opacity = 1;
+        triangle1.style.opacity = 1;
+        triangle2.style.opacity = 1;
     }
 }
 function triggerTriangleOne() {
@@ -30,10 +32,34 @@ function triggerTriangleOne() {
         localStorage.setItem("triangle_1", "true");
     }
 }
+function triggerTriangleTwo() {
+    const triangle = document.getElementById("triangle-2");
+    const surge = document.getElementById("surge-line-2");
+    const node = document.getElementById("ghost-node-3");
+
+    if (!localStorage.getItem("triangle_2")) {
+        triangle.classList.add("hacker");
+        surge.style.opacity = 1;
+        surge.style.height = "150px";
+        setTimeout(() => {
+            node.classList.add("active");
+        }, 400);
+        localStorage.setItem("triangle_2", "true");
+    }
+}
 window.onload = function () {
     if (localStorage.getItem("triangle_1")) {
-        document.getElementById("triangle-1").classList.add("hacker");
-        document.getElementById("ghost-node-1").classList.add("active");
-        document.getElementById("triangle-1").style.opacity = 1;
+        const t1 = document.getElementById("triangle-1");
+        const n1 = document.getElementById("ghost-node-1");
+        t1.classList.add("hacker");
+        n1.classList.add("active");
+        t1.style.opacity = 1;
+    }
+    if (localStorage.getItem("triangle_2")) {
+        const t2 = document.getElementById("triangle-2");
+        const n3 = document.getElementById("ghost-node-3");
+        t2.classList.add("hacker");
+        n3.classList.add("active");
+        t2.style.opacity = 1;
     }
 };

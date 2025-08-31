24162af62f3fb0c69f4aa3a4efa7340664f15253
RobertVLevesque <robertvlevesque@gmail.com>
2025-08-31 14:15:25 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,13 @@
+const nodes = document.querySelectorAll('.node');
+const button = document.getElementById('activate');
+let current = 0;
 
-const rlSigal = document.getElementById("rl-sigal");
-const nodes = [document.getElementById("node-1"), document.getElementById("node-2"), document.getElementById("node-3")];
-const triangles = [document.getElementById("triangle-1"), document.getElementById("triangle-2"), document.getElementById("triangle-3")];
-
-rlSigal.addEventListener("mouseenter", () => {
-  nodes.forEach((node, i) => {
-    setTimeout(() => {
-      node.style.opacity = 1;
-      setTimeout(() => node.style.opacity = 0.2, 300);
-    }, i * 200);
-  });
-});
-
-rlSigal.addEventListener("click", () => {
-  triangles[0].classList.add("revealed");
-});
-
-triangles[0].addEventListener("click", () => {
-  nodes[0].classList.add("activated");
+button.addEventListener('click', () => {
+  if (current < nodes.length) {
+    nodes[current].classList.add('active');
+    current++;
+  } else {
+    button.innerText = 'All Nodes Activated';
+    button.disabled = true;
+  }
 });

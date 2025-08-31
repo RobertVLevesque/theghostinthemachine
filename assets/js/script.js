91a35a1e52b39b33fca077b50895bf0b4898741b
RobertVLevesque <robertvlevesque@gmail.com>
2025-08-31 11:55:48 -0400

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,21 @@
-const nodes = document.querySelectorAll('.node');
-const button = document.getElementById('activate');
 
+const rlSigal = document.getElementById("rl-sigal");
+const nodes = [document.getElementById("node-1"), document.getElementById("node-2"), document.getElementById("node-3")];
+const triangles = [document.getElementById("triangle-1"), document.getElementById("triangle-2"), document.getElementById("triangle-3")];
 
-let current = 0;
+rlSigal.addEventListener("mouseenter", () => {
+  nodes.forEach((node, i) => {
+    setTimeout(() => {
+      node.style.opacity = 1;
+      setTimeout(() => node.style.opacity = 0.2, 300);
+    }, i * 200);
+  });
+});
 
+rlSigal.addEventListener("click", () => {
+  triangles[0].classList.add("revealed");
+});
 
-button.addEventListener('click', () => {
-if (current < nodes.length) {
-nodes[current].classList.add('active');
-current++;
-} else {
-button.innerText = 'All Nodes Activated';
-button.disabled = true;
-}
+triangles[0].addEventListener("click", () => {
+  nodes[0].classList.add("activated");
 });
